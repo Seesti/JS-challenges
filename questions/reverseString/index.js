ce1e1d70131c5aada76d9073e63a4b528117e45b
Seesti <113047119+Seesti@users.noreply.github.com>
2022-11-17 18:03:13 +0200

    updated

---
+++ questions/reverseString/index.js
@@ -0,0 +1,34 @@
+/** Reverse the provided string.
+ *
+ * @example
+ * reverse('david') === 'divad'
+ * reverse('frontend') === 'dnetnorf'
+ * reverse('Simplified!') === '!deifilpmiS'
+ */
+
+// 'David' => ['D', 'a', 'v', 'i', 'd']
+// a = '', c = 'D' => a = 'D' + '' = 'D'
+// a = 'D', c = 'a' => a = 'a' + 'D' = 'aD'
+// a = 'aD', c = 'v' => a = 'v' + 'aD' = 'vaD'
+// a = 'vaD', c = 'i' => a = 'i' + 'vaD' = 'ivaD'
+// a = 'ivaD', c = 'd' => a = 'd' + 'ivaD' = 'divaD'
+
+let str = "david"
+
+const reverseString = (str) => { 
+    return str.split("").reduce((accumulator, current) => {
+        return current + accumulator
+
+
+    }, "")
+};
+
+
+module.exports = reverseString;
+
+
+
+// const reverseString = (str) => {
+//     return str.split("").reverse().join("")
+
+// };
